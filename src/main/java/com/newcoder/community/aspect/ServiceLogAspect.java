5bb7bde66d8b1d43d9769d60bcad5d6827e7235f
yang <2411028539@qq.com>
2023-02-16 23:38:22 +0800

    第18次提交：使用AOP统一记录日志

---
+++ src/main/java/com/newcoder/community/aspect/ServiceLogAspect.java
@@ -0,0 +1,48 @@
+package com.newcoder.community.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author liuyang
+ * @create 2023-02-16 23:29
+ */
+
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* com.newcoder.community.service.*.*(..))")
+    public void pointcut(){
+
+    }
+
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint){
+        // 日志格式：用户[1.2.3.4],在[xxx],访问了[com.newcoder.community.service.xxx()].
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s],在[%s],访问了[%s].", ip, now, target));
+    }
+    
+
+
+}
--- src/test/java/com/newcoder/community/TransactionTests.java
+++ src/test/java/com/newcoder/community/TransactionTests.java
@@ -1,11 +1,17 @@
 package com.newcoder.community;
 
+import com.google.code.kaptcha.Producer;
 import com.newcoder.community.service.AlphaService;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import sun.misc.Unsafe;
 
+import java.lang.reflect.Field;
 import java.util.HashSet;
+import java.util.Objects;
 
 
 /**
